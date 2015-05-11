8c8c3532b483189f7a269329531aaa96615036c0
Momchil Hardalov <momchil.hardalov@gmail.com>
2015-05-11 21:37:44 +0300

    Made the class thread safe

--- src/main/java/org/recsyschallenge/helpers/TimeStampHelper.java
+++ src/main/java/org/recsyschallenge/helpers/TimeStampHelper.java
@@ -8,19 +8,20 @@ import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 public class TimeStampHelper {
-	private static DateFormat df;
-	private static GregorianCalendar cal;
+	private final static DateFormat df;
+	private final static TimeZone utc = TimeZone.getTimeZone("UTC");
 
 	static {
-		TimeZone utc = TimeZone.getTimeZone("UTC");
 		df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 		df.setTimeZone(utc);
-		cal = new GregorianCalendar(utc);
 	}
-	
-	private TimeStampHelper() { };
-	
-	public static Date parseDate(String input) throws ParseException {
+
+	private TimeStampHelper() {
+	};
+
+	public synchronized static Date parseDate(String input)
+			throws ParseException {
+		GregorianCalendar cal = new GregorianCalendar(utc);
 		cal.setTime(df.parse(input));
 		return cal.getTime();
 	}
